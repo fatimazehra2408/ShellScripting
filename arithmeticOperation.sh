882f0568374f10384cd654660d3de4372bc3ff73
fatimazehra2408 <55337294+fatimazehra2408@users.noreply.github.com>
2020-09-06 23:08:53 +0530

    Create arithmeticOperation.sh

---
+++ arithmeticOperation.sh
@@ -0,0 +1,21 @@
+$ A=10
+$ B=5
+$ expr $A + $B
+15
+$ expr $A - $B
+5
+$ expr $A / $B
+2
+$ expr $A \* $B
+50
+$ echo $(( A + B ))
+15
+$ echo $(( A - B ))
+5
+$ echo $(( A / B ))
+2
+$ echo $(( A * B ))
+50
+
+
+
